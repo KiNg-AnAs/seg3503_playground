8d28f4c859cf02b54c18c49b580d4fcb78bfa133
Anas El Malki <90564657+KiNg-AnAs@users.noreply.github.com>
2023-07-13 18:27:38 -0400

    lab5

---
+++ LAB5/twitter/src/Twitter.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+public class Twitter {
+
+  public String loadTweet()
+  {
+    try {
+      Thread.sleep(4000);
+    } catch (InterruptedException e) {}
+
+    double r =  Math.random();
+    if (r <= 0.45) {
+      return "I am tweet that likes to talk about @me";
+    } else if (r <= 0.9) {
+      return "Hello to @you";
+    } else {
+      return null;
+    }
+  }
+
+  public boolean isMentionned(String name) {
+    String tweet = loadTweet();
+    if (tweet == null) {return false;}
+    List<String> list = Arrays.asList(tweet.split(" "));
+    return list.contains("@" + name);
+  }
+
+}
